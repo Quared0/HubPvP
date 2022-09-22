5ef05fb83d54e164bc5b701368be9596c91340ca
Quared0 <&B6M4at8E&edkp4$>
2022-09-22 11:17:51 +1000

    Update to 1.19.2

--- src/main/java/me/quared/hubpvp/listeners/DamageListener.java
+++ src/main/java/me/quared/hubpvp/listeners/DamageListener.java
@@ -5,12 +5,13 @@ import me.quared.hubpvp.core.PvPManager;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class DamageListener implements Listener {
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGH) // WORK ON WHITELIST/BLACKLISTED WORLDS
 	public void onDamage(EntityDamageByEntityEvent e) {
 		HubPvP instance = HubPvP.instance();
 		PvPManager pvpManager = instance.pvpManager();
@@ -19,9 +20,7 @@ public class DamageListener implements Listener {
 
 			if (instance.getConfig().getStringList("disabled-worlds").contains(world.getName())) e.setCancelled(true);
 
-			if (!pvpManager.isInPvP(damager) || !pvpManager.isInPvP(damaged)) {
-				e.setCancelled(true);
-			}
+			e.setCancelled(!pvpManager.isInPvP(damager) || !pvpManager.isInPvP(damaged));
 		}
 	}
 
